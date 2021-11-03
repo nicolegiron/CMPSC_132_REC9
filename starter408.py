bb5c2917cbb286705704e6bd348551178b034e56
Nicole Giron <70114716+nicolegiron@users.noreply.github.com>
2021-11-02 22:42:34 -0400

    Add files via upload

---
+++ starter408.py
@@ -0,0 +1,19 @@
+# Recitation Activity #9
+# You must submit your file by the end of the allocated period for submissiom
+#
+# General Instructions
+#  - Do NOT change the function name
+#  - Replace  "pass" with the passcode provided during your recitation. Do not remove the quotes
+#  - Change "ans1", "ans2" with your answers. Choose a,b,c,d,e,f, or g
+#  - All answers must be strings
+
+
+def answers():
+    # Activity answers
+
+    passcode = "8944bm"
+    ans1 = "g"
+    ans2 = "c"
+
+    # Do not modify the return statement
+    return passcode, ans1, ans2
